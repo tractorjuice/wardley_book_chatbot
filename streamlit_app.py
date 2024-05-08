0956b2d46b5062dea9ae72d56875cbf167f8a0ab
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-05-08 17:39:47 +0000

    Add LangSmith Exports

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ MODEL = "gpt-3.5-turbo-1106" # 16K, Sept 2021. New Updated GPT 3.5 Turbo. The la
 
 DEBUG = True # True to overwrite files that already exist]]# Set API keys
 PINECONE_INDEX_NAME = st.secrets["PINECONE_INDEX_NAME"]
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
+os.environ["LANGCHAIN_ENDPOINT"] = st.secrets["LANGCHAIN_ENDPOINT"]
 os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
 os.environ["LANGCHAIN_ENDPOINT"] = st.secrets["LANGCHAIN_API_KEY"]
 
